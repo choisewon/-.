a3be3be057751fa81bd31226cb5dce0d6f0b726c
choisewon <chltpdnjs5@naver.com>
2019-11-04 13:33:32 +0900

    MyMouseListener 클래스를 만들었습니다.

---
+++ MyMouseLIstener.java
@@ -0,0 +1,16 @@
+import java.awt.event.*;
+/**
+ * 여기에 MyMouseLIstener 클래스 설명을 작성하십시오.
+ * 
+ * @author (작성자 이름) 
+ * @version (버전번호나 날짜)
+ */
+public class MyMouseLIstener extends MouseMotionAdapter implements MouseListener 
+{
+    public mouseClicked(MouseEvent e)
+    {
+    }
+    public mouseEntered(MouseEvent e ){}
+    
+    }
+
