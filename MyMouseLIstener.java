c55c74528545c37621677da747e2452693ea3229
iwamegu1208 <iwamegu1208@yahoo.co.jp>
2019-11-04 13:34:24 +0900

    날짜 썼습니다.

--- MyMouseLIstener.java
+++ MyMouseLIstener.java
@@ -3,7 +3,7 @@ import java.awt.event.*;
  * 여기에 MyMouseLIstener 클래스 설명을 작성하십시오.
  * 
  * @author (작성자 이름) 
- * @version (버전번호나 날짜)
+ * @version (2019.11.04)
  */
 public class MyMouseLIstener extends MouseMotionAdapter implements MouseListener 
 {
