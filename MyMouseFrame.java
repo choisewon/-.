205d898f54e1c6d7d9dbccdd5b8c6c0dba9bcb12
ayaka <ito.ayaka@gmail.com>
2019-11-04 13:14:13 +0900

    Frame 클래스 만들었어요

---
+++ MyMouseFrame.java
@@ -0,0 +1,15 @@
+import javax.swing.*;
+/**
+ * クラス MyMouseFrame の注釈をここに書きます.
+ * 
+ * @author (あなたの名前)
+ * @version (バージョン番号もしくは日付)
+ */
+public class MyMouseFrame extends JFrame
+ {  
+    public JPanel p;
+    public MyMouseFrame()
+    {
+       
+    }
+}
