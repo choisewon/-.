d626c78f6f2fbb48658e82d923e20dfd09837cc0
iwamegu1208 <iwamegu1208@yahoo.co.jp>
2019-11-04 13:41:56 +0900

    MyMouseListener를 고쳤습니다.

---
+++ MyMouseListener.java
@@ -0,0 +1,21 @@
+import java.awt.event.*;
+/**
+ * 여기에 MyMouseLIstener 클래스 설명을 작성하십시오.
+ * 
+ * @author (작성자 이름) 
+ * @version (2019.11.04)
+ */
+public class MyMouseListener extends MouseMotionAdapter implements MouseListener 
+{
+    
+    public void mouseClicked(MouseEvent e){
+        int x = e.getX();
+        int y = e.getY();
+        
+    }
+    public void mouseEntered(MouseEvent e ){}
+    public void mouseExited(MouseEvent e ){}
+    public void mousePressed(MouseEvent e ){}
+    public void mouseReleased(MouseEvent e ){}
+    }
+
